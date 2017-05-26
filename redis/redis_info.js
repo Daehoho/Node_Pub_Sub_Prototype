4407a7ba5684ad19c92abfe4523e569b1e278787
Daeho Han <supreme2705@gmail.com>
2017-05-26 17:04:22 +0900

    modified redis-config

--- redis/redis_info.js
+++ redis/redis_info.js
@@ -2,7 +2,8 @@ module.exports = (function () {
     return {
         local: {
             host: 'localhost',
-            port: '6379'
+            port: '6379',
+            password: '*daou1234!'
         },
         daou_server: {
             host: '175.115.95.51',
