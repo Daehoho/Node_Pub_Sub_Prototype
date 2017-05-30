9c53ce22edfe2c054289f75871113b0280d0e3c9
Daeho Han <supreme2705@gmail.com>
2017-05-30 14:56:09 +0900

    modified notify

--- socket.js
+++ socket.js
@@ -114,7 +114,7 @@ module.exports = function (io, pub, sub) {
         });
         g_socket.on('send_notify', function (data) {
             var data = data;
-            var msg = data.channel + ":" + "[" + getToday() + "] " + data.channel + "번 방에서 " + data.member.member_name + "에게 알림이 왔습니다";
+            var msg = data.channel + "*" + "[" + getToday() + "] " + data.channel + "번 방에서 " + data.member.member_name + "에게 알림이 왔습니다";
             pub.publish("g:" + data.channel, msg);
         });
 
